8a987729bae6cf62a6b0207ac9ed301c0f9e81c6
Abdul <AMalik@lcs.on.ca>
2020-10-05 09:09:07 -0400

    Got the runways poster partly completed just need do add loops

--- Playground.playground/Pages/Pixies post.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Pixies post.xcplaygroundpage/Contents.swift
@@ -47,62 +47,155 @@ PlaygroundPage.current.liveView = canvas
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
 
 let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
+
 let offWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
 
+
+
 // turn off boarders
+
 canvas.drawShapesWithBorders = false
 
 
+
+
+
 // Green background
+
 canvas.fillColor = limeGreen
+
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 
+
+// Text
+
+canvas.drawText(message: "pixies", at: Point(x: 40, y: 420), size: 60, kerning: 0 )
+
+
+
+canvas.drawText(message: "Saturday", at: Point(x: 40, y: 550), size: 12, kerning: 0 )
+
+
+
+canvas.drawText(message: "december 13 1986", at: Point(x: 40, y: 530), size: 12, kerning: 0 )
+
+canvas.drawText(message: "9 pm over 21", at: Point(x: 40, y: 510), size: 12, kerning: 0 )
+
+
+
+canvas.drawText(message: "at the rat", at: Point(x: 275, y: 550), size: 12, kerning: 0 )
+
+
+
+canvas.drawText(message: "528 commonwealth", at: Point(x: 275, y: 530), size: 12, kerning: 0 )
+
+canvas.drawText(message: "boston, mass.", at: Point(x: 275, y: 510), size: 12, kerning: 0 )
+
+canvas.textColor = offWhite
+
+canvas.drawText(message: "with", at: Point(x: 275, y: 470), size: 15, kerning: 0 )
+
+canvas.drawText(message: "throwing muses", at: Point(x: 275, y: 455), size: 15, kerning: 0 )
+
+canvas.drawText(message: "Big dipper", at: Point(x: 275, y: 440), size: 15, kerning: 0 )
+
+
+
+
+
+
+
 // Black background for bottom part
+
 canvas.fillColor = Color.black
+
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 400)
 
+
+
 // Draw a line of circles
+
 for y in stride(from: 0,
+
                 through: 400,
+
                 by: 40) {
+
     
+
     for x in stride(from: 0,
+
                     through: 400,
+
                     by: 40) {
+
         
+
         // code in this block is repeated 10 * 10 = 100
+
         x
+
         
+
         // Set the fill
+
         if x == 0 {
+
             canvas.fillColor = limeGreen
+
         } else if y == 0 {
+
             canvas.fillColor = limeGreen
+
         } else if y == 400 {
+
             canvas.fillColor = limeGreen
+
         } else if x == 400 {
+
             canvas.fillColor = limeGreen
+
         }  else if x + y > 400 && x != 0 && x != 400 {
+
             canvas.fillColor = limeGreen
+
         } else {
+
             canvas.fillColor = offWhite
+
             
+
         }
+
         
+
         // Draw circle
+
         canvas.drawEllipse(at: Point(x: x, y: y),
+
                            width: 35,
+
                            height: 35)
+
         
+
     }
+
     
+
 }
 
 
 
 
+
+
+
+
+
 // Draw the Axis with a scale
+
 canvas.drawAxes(withScale: true, by: 50)
 /*:
  ## Show the Assistant Editor
---
+++ Playground.playground/Pages/Post runaways.xcplaygroundpage/Contents.swift
@@ -0,0 +1,135 @@
+//: [Previous](@previous)
+
+import Foundation
+//: [Previous](@previous) / [Next](@next)
+//: # Blank canvas to copy
+//:
+//: ## To duplicate this page
+//:
+//: Place your cursor on line 19 of this page, press `Command-A` to select all text, and then `Command-C` to copy.
+//:
+//: Two-finger tap on the playground, then choose **New Playground Page**:
+//:
+//: ![new-playground](new-playground.png "New Playground")
+//:
+//: Move to the new page and press `Command-A` to select all text there, then `Command-V` to paste.
+//:
+/*:
+ ## Canvas size
+ 
+ Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
+ */
+let preferredWidth = 400
+let preferredHeight = 600
+/*:
+ ## Required code
+ 
+ Lines 28 to 36 are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+import CanvasGraphics
+
+// Create canvas
+let canvas = Canvas(width: preferredWidth, height: preferredHeight)
+
+// Show the canvas in the playground's live view
+PlaygroundPage.current.liveView = canvas
+/*:
+ ## Add your code
+ 
+ Beginning on line 48, write a meaningful comment.
+ 
+ You can remove the code on line 49 and begin writing your own code.
+ 
+ [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
+
+ */
+
+// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// COLORS
+let pink = Color(hue: 338, saturation: 83, brightness: 89, alpha: 100)
+let black = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
+let beige = Color(hue: 69, saturation: 6, brightness: 87, alpha: 100)
+
+// turn off boarders
+canvas.drawShapesWithBorders = false
+
+// pink background
+canvas.fillColor = pink
+
+// Draw background
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+// Draw line thickness
+canvas.defaultLineWidth = 12
+
+// Draw a line
+canvas.drawLine(from: Point(x: 25, y: 0), to: Point(x: 25, y: 25 ))
+canvas.drawLine(from: Point(x: 0, y: 25), to: Point(x: 25, y: 25 ))
+
+canvas.drawLine(from: Point(x: 75, y: 0), to: Point(x: 75, y: 75 ))
+canvas.drawLine(from: Point(x: 0, y: 75), to: Point(x: 75, y: 75 ))
+
+canvas.drawLine(from: Point(x: 0, y: 125), to: Point(x: 125, y: 125 ))
+canvas.drawLine(from: Point(x: 125, y: 0), to: Point(x: 125, y: 125 ))
+
+canvas.drawLine(from: Point(x: 175, y: 0), to: Point(x: 175, y: 175 ))
+canvas.drawLine(from: Point(x: 0, y: 175), to: Point(x: 175, y: 175 ))
+
+// Draw line color
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = true
+canvas.lineColor = Color.white
+
+canvas.drawLine(from: Point(x: 0, y: 50), to: Point(x: 50, y: 50 ))
+canvas.drawLine(from: Point(x: 50, y: 0), to: Point(x: 50, y: 50 ))
+
+canvas.drawLine(from: Point(x: 100, y: 0), to: Point(x: 100, y: 100 ))
+canvas.drawLine(from: Point(x: 0, y: 100), to: Point(x: 100, y: 100 ))
+
+canvas.drawLine(from: Point(x: 0, y: 150), to: Point(x: 150, y: 150 ))
+canvas.drawLine(from: Point(x: 150, y: 0), to: Point(x: 150, y: 150 ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Draw the Axis with a scale
+canvas.defaultLineWidth = 1
+canvas.drawAxes(withScale: true, by: 50)
+/*:
+ ## Show the Assistant Editor
+ Don't see any results?
+ 
+ Remember to show the Assistant Editor (1), and then switch to Live View (2):
+ 
+ ![timeline](timeline.png "Timeline")
+
+ ## Use source control
+ To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
+ 
+ Please commit and push your work often.
+ 
+ ![source_control](source-control.png "Source Control")
+ */
+
+var str = "Hello, playground"
+
+//: [Next](@next)
