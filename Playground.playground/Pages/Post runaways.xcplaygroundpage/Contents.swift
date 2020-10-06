90d525bd08191900bed7c803e6bbd61c469080cd
Abdul <AMalik@lcs.on.ca>
2020-10-06 12:08:11 -0400

    did 4 loops

--- Playground.playground/Pages/Post runaways.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Post runaways.xcplaygroundpage/Contents.swift
@@ -66,6 +66,23 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 // Draw line thickness
 canvas.defaultLineWidth = 15
 
+// Text
+canvas.drawText(message: "friday", at: Point(x: 15, y: 550), size: 10, kerning: 0 )
+canvas.drawText(message: "august 19 1977", at: Point(x: 15, y: 535), size: 10, kerning: 0 )
+canvas.drawText(message: "with", at: Point(x: 125, y: 550), size: 10, kerning: 0 )
+canvas.drawText(message: "speacial quests", at: Point(x: 125, y: 535), size: 10, kerning: 0 )
+canvas.drawText(message: "wolfgang", at: Point(x: 125, y: 520), size: 10, kerning: 0 )
+canvas.drawText(message: "atben h.lewis hall", at: Point(x: 280, y: 550), size: 10, kerning: 0 )
+canvas.drawText(message: "3443 orange st.", at: Point(x: 280, y: 535), size: 10, kerning: 0 )
+canvas.drawText(message: "riverside, california", at: Point(x: 280, y: 520), size: 10, kerning: 0 )
+
+
+
+
+canvas.textColor = Color.white
+canvas.drawText(message: "the runaways", at: Point(x: 15, y: 420), size: 50, kerning: 0 )
+
+
 // Draw a line
 //canvas.lineColor = Color.black
 //canvas.drawLine(from: Point(x: 25, y: 0), to: Point(x: 25, y: 25 ))
@@ -149,6 +166,63 @@ canvas.drawLine(from: Point(x: 400, y: 100), to: Point(x: 300, y: 100 ))
 
 canvas.drawLine(from: Point(x: 250, y: 0), to: Point(x: 250, y: 150 ))
 canvas.drawLine(from: Point(x: 400, y: 150), to: Point(x: 250, y: 150 ))
+
+
+// Draw top left side
+
+canvas.drawLine(from: Point(x: 0, y: 250), to: Point(x: 150, y: 250 ))
+canvas.drawLine(from: Point(x: 150, y: 250), to: Point(x: 150, y: 400 ))
+
+canvas.drawLine(from: Point(x: 100, y: 300), to: Point(x: 100, y: 400 ))
+canvas.drawLine(from: Point(x: 100, y: 300), to: Point(x: 0, y: 300 ))
+
+canvas.drawLine(from: Point(x: 0, y: 350), to: Point(x: 50, y: 350 ))
+canvas.drawLine(from: Point(x: 50, y: 350), to: Point(x: 50, y: 400 ))
+
+// Draw line color
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = true
+canvas.lineColor = Color.black
+
+canvas.drawLine(from: Point(x: 0, y: 225), to: Point(x: 175, y: 225 ))
+canvas.drawLine(from: Point(x: 175, y: 400), to: Point(x: 175, y: 225 ))
+
+canvas.drawLine(from: Point(x: 0, y: 275), to: Point(x: 125, y: 275 ))
+canvas.drawLine(from: Point(x: 125, y: 400), to: Point(x: 125, y: 275 ))
+
+canvas.drawLine(from: Point(x: 0, y: 325), to: Point(x: 75, y: 325 ))
+canvas.drawLine(from: Point(x: 75, y: 400), to: Point(x: 75, y: 325 ))
+
+canvas.drawLine(from: Point(x: 0, y: 375), to: Point(x: 25, y: 375 ))
+canvas.drawLine(from: Point(x: 25, y: 400), to: Point(x: 25, y: 375 ))
+
+// Draw top right
+
+canvas.drawLine(from: Point(x: 225, y: 225), to: Point(x: 400, y: 225 ))
+canvas.drawLine(from: Point(x: 225, y: 225), to: Point(x: 225, y: 400 ))
+
+canvas.drawLine(from: Point(x: 275, y: 275), to: Point(x: 400, y: 275 ))
+canvas.drawLine(from: Point(x: 275, y: 275), to: Point(x: 275, y: 400 ))
+
+canvas.drawLine(from: Point(x: 325, y: 325), to: Point(x: 400, y: 325 ))
+canvas.drawLine(from: Point(x: 325, y: 325), to: Point(x: 325, y: 400 ))
+
+canvas.drawLine(from: Point(x: 375, y: 375), to: Point(x: 400, y: 375 ))
+canvas.drawLine(from: Point(x: 375, y: 375), to: Point(x: 375, y: 400 ))
+
+// Draw line color
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = true
+canvas.lineColor = Color.white
+
+canvas.drawLine(from: Point(x: 250, y: 250), to: Point(x: 400, y: 250 ))
+canvas.drawLine(from: Point(x: 250, y: 250), to: Point(x: 250, y: 400 ))
+
+canvas.drawLine(from: Point(x: 300, y: 300), to: Point(x: 400, y: 300 ))
+canvas.drawLine(from: Point(x: 300, y: 300), to: Point(x: 300, y: 400 ))
+
+canvas.drawLine(from: Point(x: 350, y: 350), to: Point(x: 400, y: 350 ))
+canvas.drawLine(from: Point(x: 350, y: 350), to: Point(x: 350, y: 400 ))
 
 
 
